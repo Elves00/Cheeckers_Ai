634476dd3edec61f70e7f3dbf47abb8503675084
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-10-12 16:15:26 +1300

    feat: ai move returns finalRow and col, intialRow and col check for dx dy added

--- Game/guiInterface.py
+++ Game/guiInterface.py
@@ -9,17 +9,22 @@ class guiInterface():
         '''Sets up the interface'''
         self.currentBoard = board.board()
         self.currentBoard.swap_board("full")
-        
+
         self.miniMax = miniMaxAlphaBeta()
         self.miniMax.setGameBoard(self.currentBoard)
 
-
         self.moveList = []
-    
+
     def move(self, endPosY, endPosX, startPosY, startPosX, player):
         '''Performs a move'''
         dy = startPosY-endPosY
         dx = endPosX-startPosX
+
+        if (dy > 2 or dy < -2):
+            return False
+        if (dx > 4 or dx < -4 or dx == 0 or dx == 3 or dx == -3):
+            return False
+
         self.currentBoard.move(dy, dx, startPosY, startPosX)
         self.currentBoard.next_Player()
 
@@ -27,21 +32,31 @@ class guiInterface():
         '''Performs a jump'''
         dy = startPosY-endPosY
         dx = endPosX-startPosX
-        (tempRow, tempCol) = self.currentBoard.jump(
-                dy, dx, startPosY, startPosX, self.moveList)
-        self.currentBoard.human_jump(tempRow, tempCol)
+        if (dy > 2 or dy < -2):
+            return (False,0)
+        if (dx > 4 or dx < -4 or dx == 0 or dx == 3 or dx == -3):
+            return (False,0)
+        
+        (movelist, tempRow, tempCol) = self.currentBoard.jump(
+            dy, dx, startPosY, startPosX, self.moveList)
         self.currentBoard.next_Player()
+        #Returns the position the piece has moved to.
+        return (tempRow,tempCol)
 
     def is_jump(self, endPosY, endPosX, startPosY, startPosX, player):
         '''Is the move a jump'''
         dy = startPosY-endPosY
         dx = endPosX-startPosX
 
+        if (dy > 2 or dy < -2):
+            return False
+        if (dx > 4 or dx < -4 or dx == 0 or dx == 3 or dx == -3):
+            return False
+
         if (self.currentBoard.is_jump_valid(dy, dx, startPosY, startPosX, self.moveList)):
             return True
         else:
             return False
-
 
     def is_player_move_valid(self, endPosY, endPosX, startPosY, startPosX, player):
         '''
@@ -54,6 +69,11 @@ class guiInterface():
         dy = startPosY-endPosY
         dx = endPosX-startPosX
 
+        if (dy > 2 or dy < -2):
+            return False
+        if (dx > 4 or dx < -4 or dx == 0 or dx == 3 or dx == -3):
+            return False
+
         if (player != self.currentBoard.player):
             return False
 
@@ -64,16 +84,17 @@ class guiInterface():
             return False
 
     def ai_move(self):
-        self.miniMax.setGameBoard(self.currentBoard)
-        
-        turn=self.currentBoard.turn
-        (winLoss, upOrDown, leftOrRight,
-         posRow, posCol) = self.miniMax.max(-2,self.miniMax.maxValue+2)
 
-        
-        print("Max Returned:", winLoss, upOrDown,
-              leftOrRight, posRow, posCol , "Player:",self.currentBoard.player)
-        
+        self.miniMax.setGameBoard(self.currentBoard)
+
+        turn = self.currentBoard.turn
+
+        (winLoss, upOrDown, leftOrRight,
+         posRow, posCol) = self.miniMax.max(-2, self.miniMax.maxValue+2)
+        initalRow=posRow
+        initalCol=posCol
+        print("Max Returned:", winLoss, upOrDown, leftOrRight, posRow, posCol, "Player:", self.currentBoard.player)
+
         # If the AI move is a jump call the ai can move again
         if (self.currentBoard.is_jump(upOrDown, leftOrRight, posRow, posCol)):
 
@@ -81,9 +102,9 @@ class guiInterface():
             moveList = []
             while (self.currentBoard.is_jump(upOrDown, leftOrRight, posRow, posCol) and not self.currentBoard.is_end()):
                 self.currentBoard.display()
-                print(leftOrRight,upOrDown,posRow,posCol)
+                print(leftOrRight, upOrDown, posRow, posCol)
                 input()
-                
+
                 # Performs the first jump
                 (moveList, tempRow, tempCol) = self.currentBoard.jump(
                     upOrDown, leftOrRight, posRow, posCol, moveList)
@@ -93,368 +114,24 @@ class guiInterface():
 
                 # Runs jumping max with previous move list
                 (winLoss, upOrDown, leftOrRight,
-                    posRow, posCol) = self.miniMax.jumping_max(tempRow, tempCol, moveList,-2,self.miniMax.maxValue+2)
+                    posRow, posCol) = self.miniMax.jumping_max(tempRow, tempCol, moveList, -2, self.miniMax.maxValue+2)
                 # If the AI retuned none it means there is no moves for the jumping piece without doubling back
                 if (posRow == None or posCol == None):
+                    posRow=tempRow
+                    posCol=tempCol
                     break
         else:
-            self.currentBoard.move(upOrDown, leftOrRight, posRow, posCol)
+            (posRow,posCol)=self.currentBoard.move(upOrDown, leftOrRight, posRow, posCol)
 
-        #Hard resets the turn
-        self.currentBoard.turn=turn
+        # Hard resets the turn
+        self.currentBoard.turn = turn
         self.currentBoard.next_Player()
+        finalRow=posRow
+        finalCol=posCol
+        return (finalRow,finalCol,initalRow,initalCol)
 
     def getCurrentBoard(self):
         return self.currentBoard
 
     def setCurrentBoard(self, board):
         self.currentBoard = board
-
-
-x = guiInterface()
-if (x.is_player_move_valid(5, 4, 6, 3, 'R')):
-    print("Works for small board")
-x.currentBoard.display()
-# print(x.currentBoard.player)
-# print(x.currentBoard.board[3][13])
-# if (x.is_player_move_valid(4, 14, 3, 13, 'R')):
-#     print("Works for full board")
-# x.currentBoard.display()
-# if (x.is_player_move_valid(4, 14, 3, 13, 'R')):
-#     raise Exception("Doesn't work here")
-# else:
-#     print("No longer the player so returns false")
-
-x.currentBoard.display()
-#R
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#G
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#P
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#B
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#O
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#Y
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#R
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#R
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#G
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#P
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#B
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#O
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#Y
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#R
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#R
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#G
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#P
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#B
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#O
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#Y
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#R
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#R
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#G
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#P
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#B
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#O
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#Y
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#R
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#R
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#G
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#P
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#B
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-#O
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#Y
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#R
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#G
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-x.currentBoard.display()
-input()
-
-#P
-x.ai_move()
-x.currentBoard.display()
-#B
-input()
-x.ai_move()
-input()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-input()
-print("layer:",x.currentBoard.player)
-input()
-x.ai_move()
-input()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
-x.ai_move()
-x.currentBoard.display()
